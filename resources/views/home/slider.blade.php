3f67dc93b64272f2cea0990caaec22f53294edfd
unknown <dwifrizal24@gmail.com>
2025-03-24 09:11:41 +0700

    first commit

---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,34 @@
+<section class="slider_section">
+      <div class="slider_container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box"> 
+                      <h1 class="text_inno">
+                        INNO X3 <br>
+                      </h1>
+                    <p class="text_inno">
+                    INNO X3 PRO delivers 8KHz wireless speed and pixel-perfect precision with the PixArt PAW 3950 MAX sensor. Equipped with Omron D2FP-FN2 optical switches and tri-mode connectivity, it’s built for ultimate responsiveness and control. Lightweight, ergonomic, and ready to dominate.
+                    </p>
+                      <a href="">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box">
+                      <img style="width:600px" src="images/innox3jepun.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          
+        </div>
+      </div>
+    </section>
